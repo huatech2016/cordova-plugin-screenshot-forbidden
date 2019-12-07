09013322070fb80027c42635663a66e3dca9adee
hustme <hustme@126.com>
2019-12-07 23:06:35 +0800

    导出空函数

--- www/ScreenshotForbidden.js
+++ www/ScreenshotForbidden.js
@@ -3,3 +3,8 @@ var exec = require('cordova/exec');
 document.addEventListener('deviceready', function () {
     exec(null, null, 'ScreenshotForbidden', null,null);
 }, false);
+
+
+exports.status = function (arg0, success, error) {
+    
+};
