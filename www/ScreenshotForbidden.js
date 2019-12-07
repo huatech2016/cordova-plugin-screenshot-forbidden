020234bc3e55cc9597fc1155bc8561b868cd42ee
hustme <hustme@126.com>
2019-12-07 23:11:31 +0800

    加载js模块，以便自动禁止

--- www/ScreenshotForbidden.js
+++ www/ScreenshotForbidden.js
@@ -1,10 +1,3 @@
-var exec = require('cordova/exec');
-
 document.addEventListener('deviceready', function () {
     exec(null, null, 'ScreenshotForbidden', null,null);
 }, false);
-
-
-exports.status = function (arg0, success, error) {
-    
-};
